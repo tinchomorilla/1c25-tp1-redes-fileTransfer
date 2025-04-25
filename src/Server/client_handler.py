2980c5d1bc2cff184db1ba50e3acbd80e44bc1d1
tinchomorilla <mmorilla@fi.uba.ar>
2025-04-25 16:41:52 -0300

    add some debug prints

--- src/Server/client_handler.py
+++ src/Server/client_handler.py
@@ -4,15 +4,17 @@ from src.RDT.stop_and_wait import StopAndWaitRDT
 DONE_MARKER = b"__UPLOAD_DONE__"
 
 class ClientHandler:
-    def __init__(self, sock, addr, storage_dir, filename):
+    def __init__(self, sock, addr, storage_dir, init_msg):
         self.sock = sock  # Socket compartido con el servidor
         self.addr = addr  # Dirección del cliente (IP y puerto)
         self.storage_dir = storage_dir  # Directorio de almacenamiento en el servidor
-        self.filename = filename  # Nombre del archivo asociado a la operación
+        self.filename = init_msg.decode().split("|")[1]
+      
 
+        
     def handle(self):
         """Maneja la lógica de comunicación con un cliente."""
-        print(f"[HANDLE_CLIENTS] Atendiendo cliente: {self.addr}, archivo: {self.filename}")
+        print(f"[Client Handler] Atendiendo cliente: {self.addr}, archivo: {self.filename}")
         self._receive_file()
 
     def _receive_file(self):
@@ -21,12 +23,18 @@ class ClientHandler:
         filepath = os.path.join(self.storage_dir, self.filename)
 
         with open(filepath, "wb") as f:
-            print(f"[HANDLE_CLIENTS] Recibiendo archivo: {self.filename}")
+            print(f"[Client Handler] Recibiendo archivo: {self.filename}")
             while True:
                 data, _ = rdt.recv()
+                if data.startswith(b"UPLOAD|"):
+                    print(f"[DEBUG] Ignorando mensaje inicial: {data}")
+                    continue
                 if data == DONE_MARKER:
-                    print(f"[HANDLE_CLIENTS] Fin de transmisión recibido de {self.addr}")
+                    print(f"[Client Handler] Fin de transmisión recibido de {self.addr}")
+                    print(f"[Client Handler] Recibiendo fragmento de {len(data)} bytes")
+
                     break
+                print(f"[Client Handler] Recibiendo fragmento de {len(data)} bytes")
                 f.write(data)
 
-        print(f"[HANDLE_CLIENTS] Archivo recibido: {filepath}")
+        print(f"[Client Handler] Archivo recibido: {filepath}")
