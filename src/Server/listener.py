956b506303a4d43a19f23d983c472c215f9cdfc4
tinchomorilla <mmorilla@fi.uba.ar>
2025-04-26 18:39:28 -0300

    integrate packet queue and assign each client its own RDT protocol

--- src/Server/listener.py
+++ src/Server/listener.py
@@ -1,17 +1,44 @@
+import sys
+from os.path import abspath, dirname
+import threading
+from src.RDT.stop_and_wait import StopAndWaitRDT
 from src.Server.client_handler import ClientHandler
+sys.path.insert(0, abspath(dirname(dirname(dirname(__file__)))))
+
+import queue
+
+lock = threading.Lock()
+
 
 class Listener:
     def __init__(self, sock):
         self.sock = sock
+        self.handlers = {}
 
     def listen(self, storage_dir):
         """Escucha conexiones entrantes y delega el manejo a handle_client."""
         while True:
-            try:
-                data, addr = self.sock.recvfrom(1024)
-                print(f"[LISTENER] Atendiendo nuevo cliente: {addr}")
-                client = ClientHandler(self.sock, addr, storage_dir, data)
-                client.handle()                
-            except KeyboardInterrupt:
-                print("[LISTENER] Cerrando servidor")
-                break
+            data, addr = self.sock.recvfrom(1024)
+            with lock:
+                if addr not in self.handlers:
+                    q = queue.Queue()
+                    self.handlers[addr] = q
+                    threading.Thread(
+                        target=self.handle_client,
+                        args=(self.sock, addr, q, storage_dir),
+                        daemon=True,
+                    ).start()
+
+                self.handlers[addr].put(data)
+
+    def handle_client(self, sock, addr, q, storage_dir):
+        try:
+            rdt = StopAndWaitRDT(sock, addr=addr, queue=q)
+            client = ClientHandler(rdt, storage_dir)
+            client.handle()
+        except Exception as e:
+            print(f"[SERVER] Error procesando cliente {addr}: {e}")
+        finally:
+            with lock:
+                if addr in self.handlers:
+                    del self.handlers[addr]
